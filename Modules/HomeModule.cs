ea305df880fc30b6aa41e91013c376219f732282
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-05-26 10:02:03 -0700

    add changes to root page; add a new webpage designated towards the user input form

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,6 +10,9 @@ namespace Contact.Object
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/new"] = _ => {
+        return View["contact_form.cshtml"];
+      };
       Post["/contacts/new"] = _ => {
         Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-number"], Request.Form["new-address"]);
         newContact.AddToList();
