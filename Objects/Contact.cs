85e7f046121f2c292ce3a4e0815b4fe68479b995
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-05-26 08:53:46 -0700

    add private variables, constructor, and 'get' methods

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -3,4 +3,29 @@ using System.Collections.Generic;
 namespace Contact.Object
 {
   public class Contact
+  {
+    private string _name;
+    private string _number;
+    private string _address;
+
+    public Contact(string newName, string newNumber, string newAddress)
+    {
+      _name = newName;
+      _number = newNumber;
+      _address = newAddress;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public string GetNumber()
+    {
+      return _number;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+  }
 }
