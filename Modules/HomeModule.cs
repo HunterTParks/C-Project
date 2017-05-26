cd7d9a607da931f9c36dfec01ceffbd9bced7253
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-05-26 08:30:15 -0700

    add basic working template for running a server; also contains first draft for an input form from the user

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace project
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
